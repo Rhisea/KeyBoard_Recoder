facbb00dbb6262601a1f914a0c8591c45e494dff
jijiji-jxj <69882559+jijiji-jxj@users.noreply.github.com>
2023-10-27 11:53:17 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,163 +1,192 @@
 #include <bits/stdc++.h>
-#include <conio.h>
 #include <Windows.h>
+#define endl '\n'
 using namespace std;
+inline int read(){
+    int x = 0,f = 1;
+    char c = getchar();
+    while (!isdigit(c)){
+        if (c == '-') f = -1;
+        c = getchar();
+    }
+    while (isdigit(c)){
+        x = x*10+c-'0';
+        c = getchar();
+    }
+    return x*f;
+}
 int bs,Tab,enter,shift,ctrl,alt,caps,esc,space,page_up,page_down;
-int end,home,left_arrow,up_arrow,right_arrow,dw_arrow,insert,del,num_lock;
+int End,home,left_arrow,up_arrow,right_arrow,dw_arrow,insert,del,num_lock;
 int fenmao,dengjia,douxiaoyu,jianxiahua,diandayu,xiewenhao,bolangxian;
 int zuozhongdakuohao,fanxiehuo,youzhongdakuohao,yinghao,A,B,C,D,E,F;
-int G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
+int G,H,I,J,K,L,M,N,O,P,Q,R,S,TT = 0,U,V,W,X,Y,Z;
 int zero,one,two,three,four,five,six,seven,eight,nine;
+SYSTEMTIME systime;
+LRESULT CALLBACK Proc(int code,WPARAM w,LPARAM l);
 signed main(){
-	int key;
 	ShowWindow(GetConsoleWindow(),SW_MINIMIZE); 
-	while(1){
-		key = 0;
-		ofstream fout("Recoder.txt");
-		fout<<"bs:"<<bs<<endl<<"Tab:"<<Tab<<endl<<"enter:"<<enter<<endl<<"shift:"<<shift<<endl<<"ctrl:"<<ctrl<<endl<<"alt:"<<alt<<endl<<"caps:"<<caps<<endl;
-		fout<<"esc:"<<esc<<endl<<"space:"<<space<<endl<<"page_up:"<<page_up<<endl<<"page_down:"<<page_down<<endl<<"end:"<<end<<endl<<"home:"<<home<<endl<<"left_arrow:"<<left_arrow<<endl;
+	TT = 0;
+    HHOOK hook = ::SetWindowsHookEx(WH_KEYBOARD_LL,Proc,0,0);
+    MSG msg;
+    while (GetMessage(&msg,NULL,0,0)){
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    UnhookWindowsHookEx(hook);
+    return 0;
+} 
+LRESULT CALLBACK Proc(int code,WPARAM w,LPARAM l){
+	ofstream fout("Recoder.txt");
+    KBDLLHOOKSTRUCT *T = (KBDLLHOOKSTRUCT*)l;
+    DWORD x = T->vkCode;
+    if (w == WM_KEYDOWN){
+        GetLocalTime(&systime);
+        fout<<"bs:"<<bs<<endl<<"Tab:"<<Tab<<endl<<"enter:"<<enter<<endl<<"shift:"<<shift<<endl<<"ctrl:"<<ctrl<<endl<<"alt:"<<alt<<endl<<"caps:"<<caps<<endl;
+		fout<<"esc:"<<esc<<endl<<"space:"<<space<<endl<<"page_up:"<<page_up<<endl<<"page_down:"<<page_down<<endl<<"end:"<<End<<endl<<"home:"<<home<<endl<<"left_arrow:"<<left_arrow<<endl;
 		fout<<"up_arrow:"<<up_arrow<<endl<<"right_arrow:"<<right_arrow<<endl<<"dw_arrow:"<<dw_arrow<<endl<<"insert:"<<insert<<endl<<"del:"<<del<<endl<<"num_lock:"<<num_lock<<endl<<"fenmao:"<<fenmao<<endl;
 		fout<<"dengjia:"<<dengjia<<endl<<"douxiaoyu:"<<douxiaoyu<<endl<<"jianxiahua:"<<jianxiahua<<endl<<"diandayu:"<<diandayu<<endl<<"xiewenhao:"<<xiewenhao<<endl<<"bolangxian:"<<bolangxian<<endl;
-		fout<<"zuozhongdakuohao"<<zuozhongdakuohao<<endl<<"fanxiehuo:"<<fanxiehuo<<endl<<"youzhongdakuohao:"<<youzhongdakuohao<<endl<<"yinghao:"<<yinghao<<endl;
+		fout<<"zuozhongdakuohao:"<<zuozhongdakuohao<<endl<<"fanxiehuo:"<<fanxiehuo<<endl<<"youzhongdakuohao:"<<youzhongdakuohao<<endl<<"yinghao:"<<yinghao<<endl;
 		fout<<"A:"<<A<<endl<<"B:"<<B<<endl<<"C:"<<C<<endl<<"D:"<<D<<endl<<"E"<<E<<endl<<"F:"<<F<<endl<<"G:"<<G<<endl<<"H:"<<H<<endl<<"I:"<<I<<endl<<"J:"<<J<<endl;
-		fout<<"K:"<<K<<endl<<"L:"<<L<<endl<<"M:"<<M<<endl<<"N:"<<N<<endl<<"O:"<<O<<endl<<"P:"<<P<<endl<<"Q:"<<Q<<endl<<"R:"<<R<<endl<<"S"<<S<<endl<<"T:"<<T<<endl;
+		fout<<"K:"<<K<<endl<<"L:"<<L<<endl<<"M:"<<M<<endl<<"N:"<<N<<endl<<"O:"<<O<<endl<<"P:"<<P<<endl<<"Q:"<<Q<<endl<<"R:"<<R<<endl<<"S:"<<S<<endl<<"T:"<<TT<<endl;
 		fout<<"U:"<<U<<endl<<"V:"<<V<<endl<<"W:"<<W<<endl<<"X:"<<X<<endl<<"Y:"<<Y<<endl<<"Z:"<<Z<<endl;
-		key = _getch();
-		if (key == 8)
+//        printf("Press: ");
+//        putchar(x);
+        if (x == 8)
 			bs++;
-		else if (key == 9)
+		else if (x == 9)
 			Tab++;
-		else if (key == 13)
+		else if (x == 13)
 			enter++; 
-		else if (key == 16)
+		else if (x == 16)
 			shift++;
-		else if (key == 17)
+		else if (x == 17)
 			ctrl++;
-		else if (key == 18)
+		else if (x == 18)
 			alt++;
-		else if (key == 20)
+		else if (x == 20)
 			caps++; 
-		else if (key == 27)
+		else if (x == 27)
 			esc++;
-		else if (key == 32)
+		else if (x == 32)
 			space++;
-		else if (key == 33)
+		else if (x == 33)
 			page_up++;
-		else if (key == 34)
+		else if (x == 34)
 			page_down++;
-		else if (key == 35)
-			end++;
-		else if (key == 36)
+		else if (x == 35)
+			End++;
+		else if (x == 36)
 			home++;
-		else if (key == 37)
+		else if (x == 37)
 			left_arrow++;
-		else if (key == 38)
+		else if (x == 38)
 			up_arrow++; 
-		else if (key == 39)
+		else if (x == 39)
 			right_arrow++;
-		else if (key == 40)
+		else if (x == 40)
 			dw_arrow++;
-		else if (key == 45)
+		else if (x == 45)
 			insert++;
-		else if (key == 46)
+		else if (x == 46)
 			del++; 
-		else if (key == 144)
+		else if (x == 144)
 			num_lock++;
-		else if (key == 186)
+		else if (x == 186)
 			fenmao++;
-		else if (key == 187)
+		else if (x == 187)
 			dengjia++;
-		else if (key == 188)
+		else if (x == 188)
 			douxiaoyu++; 
-		else if (key == 189)
+		else if (x == 189)
 			jianxiahua++;
-		else if (key == 190)
+		else if (x == 190)
 			diandayu++;
-		else if (key == 191)
+		else if (x == 191)
 			xiewenhao++;
-		else if (key == 192)
+		else if (x == 192)
 			bolangxian++; 
-		else if (key == 219)
+		else if (x == 219)
 			zuozhongdakuohao++;
-		else if (key == 220)
+		else if (x == 220)
 			fanxiehuo++;
-		else if (key == 221)
+		else if (x == 221)
 			youzhongdakuohao++;
-		else if (key == 222)
+		else if (x == 222)
 			yinghao++; 
-		else if (key == 97 or key == 65)
+		else if (x == 97 or x == 65){
 			A++;
-		else if (key == 98 or key == 66)
+        }
+		else if (x == 98 or x == 66)
 			B++;
-		else if (key == 99 or key == 67)
+		else if (x == 99 or x == 67)
 			C++;
-		else if (key == 100 or key == 68)
+		else if (x == 100 or x == 68)
 			D++;
-		else if (key == 69 or key == 101)
+		else if (x == 69 or x == 101)
 			E++;
-		else if (key == 70 or key == 102)
+		else if (x == 70 or x == 102)
 			F++;
-		else if (key == 71 or key == 103)
+		else if (x == 71 or x == 103)
 			G++;
-		else if (key == 72 or key == 104)
+		else if (x == 72 or x == 104)
 			H++;
-		else if (key == 73 or key == 105)
+		else if (x == 73 or x == 105)
 			I++;
-		else if (key == 74 or key == 106)
+		else if (x == 74 or x == 106)
 			J++;
-		else if (key == 75 or key == 107)
+		else if (x == 75 or x == 107)
 			K++;
-		else if (key == 76 or key == 108)
+		else if (x == 76 or x == 108)
 			L++;
-		else if (key == 77 or key == 109)
+		else if (x == 77 or x == 109)
 			M++;
-		else if (key == 78 or key == 110)
+		else if (x == 78 or x == 110)
 			N++;
-		else if (key == 79 or key == 111)
+		else if (x == 79 or x == 111)
 			O++;
-		else if (key == 80 or key == 112)
+		else if (x == 80 or x == 112)
 			P++;
-		else if (key == 81 or key == 113)
+		else if (x == 81 or x == 113)
 			Q++;
-		else if (key == 82 or key == 114)
+		else if (x == 82 or x == 114)
 			R++;
-		else if (key == 83 or key == 115)
+		else if (x == 83 or x == 115)
 			S++;
-		else if (key == 84 or key == 116)
-			T++;
-		else if (key == 85 or key == 117)
+		else if (x == 84 or x == 116)
+			TT++;
+		else if (x == 85 or x == 117)
 			U++;
-		else if (key == 86 or key == 118)
+		else if (x == 86 or x == 118)
 			V++;
-		else if (key == 87 or key == 119)
+		else if (x == 87 or x == 119)
 			W++;
-		else if (key == 88 or key == 120)
+		else if (x == 88 or x == 120)
 			X++;
-		else if (key == 89 or key == 121)
+		else if (x == 89 or x == 121)
 			Y++;
-		else if (key == 90 or key == 122)
+		else if (x == 90 or x == 122)
 			Z++;
-		else if (key == 48)
+		else if (x == 48)
 			zero++;
-		else if (key == 49)
+		else if (x == 49)
 			one++;
-		else if (key == 50)
+		else if (x == 50)
 			two++;
-		else if (key == 51)
+		else if (x == 51)
 			three++;
-		else if (key == 52)
+		else if (x == 52)
 			four++;
-		else if (key == 53)
+		else if (x == 53)
 			five++;
-		else if (key == 54)
+		else if (x == 54)
 			six++;
-		else if (key == 55)
+		else if (x == 55)
 			seven++;
-		else if (key == 56)
+		else if (x == 56)
 			eight++;
-		else if (key == 57)
+		else if (x == 57)
 			nine++;
-	}
-	return 0;
+//        cout<<endl;
+    }
+    return CallNextHookEx(NULL,code,w,l);
 }
-
